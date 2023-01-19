96405cd5a32049181d9471de472d5ff5a8aedb96
ducminhsw <ferociies@gmail.com>
2023-01-19 12:56:23 +0700

    some new ulti files

---
+++ ultis/Constants.js
@@ -0,0 +1,13 @@
+// report reasons:
+export const REPORT_NHAYCAM = 0;
+export const REPORT_LAMPHIEN = 1;
+export const REPORT_LUADAO = 2;
+export const REPORT_KHAC = 3;
+
+// genders:
+export const MALE = "male";
+export const FEMALE = "female";
+export const NO_GENDER = "secret";
+
+// files:
+export const BaseURL = "http://localhost:8000/files/"
