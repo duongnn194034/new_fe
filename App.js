2d8e3fe89060c584bb3cd27f5d023580635b11aa
ducminhsw <ferociies@gmail.com>
2023-01-19 15:18:06 +0700

    update App.js

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useFonts } from 'expo-font';
+import React, { useReducer } from 'react';
+
+import { reducer } from './context/AppReducer'
+import AppContext from './context/AppContext'
+
+import ProfileScreen from './screens/ProfileScreen';
+import EditScreen from './screens/EditScreen';
+import SettingScreen from './screens/SettingScreen';
+import ProfileViewScreen from './screens/ProfileViewScreen';
+import FriendListScreen from './screens/FriendListScreen';
+import ChangePassScreen from './screens/ChangePassScreen';
+import ChangeNameScreen from './screens/ChangeNameScreen';
+
+const Stack = createStackNavigator()
 
 export default function App() {
+  const initLoginState = {
+    token: null,
+    user_id: null,
+    isLoading: false,
+  }
+
+  const [loginState, dispatch] = useReducer(reducer, initLoginState);
+  const appContext = {
+    loginState,
+    dispatch
+  }
+
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  });
+
+  if (!loaded) return null;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppContext.Provider value={appContext}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: true }}
+          initialRouteName="Profile">
+          <Stack.Screen name="Profile" component={ProfileScreen} />
+          <Stack.Screen name="Edit" component={EditScreen} />
+          <Stack.Screen name="Setting" component={SettingScreen} />
+          <Stack.Screen name="ProfileView" component={ProfileViewScreen} />
+          <Stack.Screen name="FriendList" component={FriendListScreen} />
+          <Stack.Screen name="ChangePass" component={ChangePassScreen} />
+          <Stack.Screen name="ChangeName" component={ChangeNameScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AppContext.Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
