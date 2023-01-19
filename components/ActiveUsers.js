be2afd7214647b4a2df9cccb9e2904959da92355
Le Minh Hoang <82262672+hoang-le-minh@users.noreply.github.com>
2023-01-19 17:07:15 +0700

    chat

---
+++ components/ActiveUsers.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
+import { responsiveFontSize, responsiveHeight } from "react-native-responsive-dimensions";
+
+const ActiveUsers = ({name, profile, navigation}) => {
+
+    const onPressHanlder = () => {
+        navigation.navigate('ChatView')
+    }
+
+    return (
+        <TouchableOpacity onPress={onPressHanlder} activeOpacity={0.7} style={styles.container}>
+            <View style={styles.imageContainer}>
+                <Image style={styles.image} source={{ uri: profile }} />
+                {/* <View style={styles.activeStatus} /> */}
+            </View>
+            <Text style={styles.name}>{name}</Text>
+        </TouchableOpacity>
+    )
+}
+
+export default ActiveUsers
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        marginVertical: 10,
+        paddingHorizontal: 10,
+        alignItems: 'center'
+    },
+    imageContainer: {
+        position: 'relative'
+    },
+    image: {
+        width: responsiveHeight(5),
+        height: responsiveHeight(5),
+        borderRadius: 200
+    },
+    name: {
+        paddingHorizontal: 10,
+        fontSize: responsiveFontSize(1.8),
+        fontWeight: 'bold'
+    },
+    // activeStatus: {
+    //     position: 'absolute',
+    //     width: responsiveHeight(2),
+    //     height: responsiveHeight(2),
+    //     backgroundColor: '#03C003',
+    //     bottom: -2,
+    //     right: -2,
+    //     borderRadius: 200,
+    //     borderColor: 'white',
+    //     borderWidth: 2
+    // }
+})
